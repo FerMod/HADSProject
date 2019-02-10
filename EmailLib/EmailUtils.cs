6d2f6c135a31de816706578f156617b88b692fe3
FerMod <tudelaigarcia@gmail.com>
2019-02-10 18:13:15 +0100

    Add EmaiLib

---
+++ EmailLib/EmailUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace EmailLib {
+
+	public static class EmailUtils {
+
+		public static void SendEmail(string email, string subject, string body) {
+
+			MailMessage mail = new MailMessage("you@yourcompany.com", email) {
+				Subject = subject,
+				Body = body
+			};
+
+			SmtpClient client = new SmtpClient {
+				Port = 25,
+				DeliveryMethod = SmtpDeliveryMethod.Network,
+				UseDefaultCredentials = false,
+				Host = "smtp.ehu.eus"
+			};
+
+			client.Send(mail);
+
+		}
+
+	}
+
+}
