098b39d868c11b6329779bae44db129c0350b1da
FerMod <tudelaigarcia@gmail.com>
2019-02-16 20:54:14 +0100

    Add query file

---
+++ WebApplication/Config/CreateTableUsuarios.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Usuarios] (
+    [email]      NVARCHAR (50) NOT NULL,
+    [nombre]     NVARCHAR (50) NOT NULL,
+    [apellidos]  NVARCHAR (70) NOT NULL,
+    [numconfir]  INT           NOT NULL,
+    [confirmado] BIT           NOT NULL,
+    [tipo]       NVARCHAR (50) NOT NULL,
+    [pass]       NVARCHAR (50) NOT NULL,
+    [codpass]    INT,
+    CONSTRAINT [PK__Usuarios__0000000000000095] PRIMARY KEY CLUSTERED ([email] ASC)
+);
