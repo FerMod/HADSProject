a9bef9f47a3219f34239e87e353f0c4d53eaa38c
FerMod <tudelaigarcia@gmail.com>
2019-02-19 21:48:32 +0100

    Update table creation sql command

--- WebApplication/Config/CreateTableUsuarios.sql
+++ WebApplication/Config/CreateTableUsuarios.sql
@@ -3,9 +3,10 @@
     [nombre]     NVARCHAR (50) NOT NULL,
     [apellidos]  NVARCHAR (70) NOT NULL,
     [numconfir]  INT           NOT NULL,
-    [confirmado] BIT           NOT NULL,
+    [confirmado] BIT           NOT NULL DEFAULT 0,
     [tipo]       NVARCHAR (50) NOT NULL,
     [pass]       NVARCHAR (50) NOT NULL,
-    [codpass]    INT,
+    [codpass]    INT           NULL,
     CONSTRAINT [PK__Usuarios__0000000000000095] PRIMARY KEY CLUSTERED ([email] ASC)
 );
+
