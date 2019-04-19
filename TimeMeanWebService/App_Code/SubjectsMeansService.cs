6424b6be7b5184e2a6932a4b8d04c54cd704ce97
FerMod <tudelaigarcia@gmail.com>
2019-04-19 19:51:02 +0200

    Add subject means service

---
+++ TimeMeanWebService/App_Code/SubjectsMeansService.cs
@@ -0,0 +1,47 @@
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+using DataBaseAccess;
+
+/// <summary>
+/// Summary description for SubjectsMeansService
+/// </summary>
+//[WebService(Namespace = "http://localhost:50253/")]
+[WebService(Namespace = "http://localhost:50253/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX
+[ScriptService]
+public class SubjectsMeansService : WebService {
+
+	private readonly DataAccessService dataAccess;
+
+	public SubjectsMeansService() {
+
+		dataAccess = new DataAccessService(ConfigurationManager.ConnectionStrings["HADS18-DB"].ConnectionString);
+
+	}
+
+	[WebMethod]
+	public DataTable GetSubjectsMeans() {
+
+		string query = "SELECT ET.Email, AVG(ET.HReales) as MediaHReales, TG.CodAsig " +
+						"FROM EstudiantesTareas ET " +
+						"JOIN TareasGenericas TG " +
+						"ON ET.CodTarea = TG.Codigo " +
+						"GROUP BY ET.Email, TG.CodAsig";
+
+
+		DataTable dataTable = dataAccess.CreateQueryDataTable(query);
+		dataTable.TableName = "SubjectsMeans";
+
+		return dataTable;
+	}
+
+}
